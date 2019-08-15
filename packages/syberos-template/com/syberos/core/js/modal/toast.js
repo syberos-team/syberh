3643fae3001532e00a00adb9145041f4bac358e7
hongfei_guo_dev <guohongfei@cmos.com>
2019-08-15 18:35:00 +0800

    feat(template): 增加stoast控件

---
+++ packages/syberos-template/com/syberos/core/js/modal/toast.js
@@ -0,0 +1,36 @@
+/* eslint-disable no-undef */
+
+/***
+ * modal.alert(Object)
+ */
+function Alert () {
+  var defaultOpts = {
+    id: 'alert',
+    name: 'alert',
+    module: 'modal',
+    methods: ['alert'],
+    source: '../qml/SAlert.qml'
+  }
+  SyberPlugin.call(this, defaultOpts)
+
+  var that = this
+  this.on('alert', function (object, handlerId, param) {
+    var component = object || that.object
+
+    console.log('\n')
+    console.log('alert request', component.z)
+    console.log('\n', param)
+    component.titleText = that.param.title
+    component.messageText = that.param.message
+    component.show()
+
+//    // 确认事件
+    component.accepted.connect(function () {
+      WEBVIEWCORE.trigger('success', that.handlerId, { confirm: true })
+      console.log('-----alert accepted')
+    })
+
+  })
+}
+
+Alert.prototype = SyberPlugin.prototype
