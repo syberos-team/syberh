e564c1ebd1b2f2276615441e7057b3cd9c816f06
hongfei_guo_dev <guohongfei@cmos.com>
2019-08-22 20:06:36 +0800

    feat(template): 优化获取系统信息的方法

--- packages/syberos-template/com/syberos/api/js/system/system.js
+++ packages/syberos-template/com/syberos/api/js/system/system.js
@@ -12,9 +12,6 @@ function System () {
   }
   SyberPlugin.call(this, defaultOpts)
 
-  // 是否第一次绑定接受信号
-  this.firstConnect = false
-
   var that = this;
   this.on('aboutPhone', function (object, handlerId, param) {
     var component = object || that.object
@@ -24,14 +21,8 @@ function System () {
     info.windowWidth = gScreenInfo.displayWidth;
     info.windowHeight = gScreenInfo.displayHeight;
 
-    // 只做一次信号绑定,防止多次信号被触发
-    if(!that.firstConnect) {
-        // 设置绑定信号
-        that.firstConnect = true;
-
-        // 此处必须用that.xx ，因为后续的参数不会被传到该方法范围内
-        WEBVIEWCORE.trigger('success', that.handlerId, info);
-    }
+    // 此处必须用that.xx ，因为后续的参数不会被传到该方法范围内
+    WEBVIEWCORE.trigger('success', that.handlerId, info);
   });
 }
 
