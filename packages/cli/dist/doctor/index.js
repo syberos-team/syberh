6a07a7cfec04a0e5e1c4478866a0741014a1af71
hanagm <274506356@qq.com>
2019-09-18 16:16:27 +0800

    chore: 优化doctor

--- packages/cli/dist/doctor/index.js
+++ packages/cli/dist/doctor/index.js
@@ -3,6 +3,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const pdkSdkValidator_1 = require("./pdkSdkValidator");
 exports.default = {
     validators: [
-        pdkSdkValidator_1.default,
+        pdkSdkValidator_1.default
+        // configValidator,
+        // packageValidator
+        // recommandValidator,
+        // eslintValidator
     ]
 };
