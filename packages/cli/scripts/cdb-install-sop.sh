f2fc4696e88384b580f35b988479ec63f59a019d
liujingpeng <liujingpeng@syberos.com>
2019-09-25 19:13:54 +0800

    fix(cli): 修改cdb安装sop包脚本

--- packages/cli/scripts/cdb-install-sop.sh
+++ packages/cli/scripts/cdb-install-sop.sh
@@ -5,14 +5,19 @@ set cdbDevice [lindex $argv 1]
 set sopFilename [lindex $argv 2]
 
 set timeout 1000
+
 spawn $cdbPath -s $cdbDevice shell
 expect {
-  "root@" {send "su install\r"}
+  "developer@" {send "exit\r"; exp_continue}
+  "root@" {send "su install\r"; exp_continue}
+  "install@" {send "/usr/bin/ins-tool -siu /tmp/$sopFilename && exit\r"; exp_continue}
+  "root@" {send "rm -f /tmp/$sopFilename && exit\r"} exit
 }
+
+spawn $cdbPath -s $cdbDevice shell "/usr/bin/ins-tool -siu /tmp/$sopFilename"
 expect {
-  "install@" {send "/usr/bin/ins-tool -siu /tmp/$sopFilename && exit\r"}
-}
-expect {
+  "root@" {send "su install\r"; exp_continue}
+  "install@" {send "/usr/bin/ins-tool -siu /tmp/$sopFilename && exit\r"; exp_continue}
   "root@" {send "rm -f /tmp/$sopFilename && exit\r"}
 }
 expect eof
