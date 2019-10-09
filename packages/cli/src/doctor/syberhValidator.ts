77b7ecb669c28f5baedd242e507e7f4893cb8356
liujingpeng <liujingpeng@syberos.com>
2019-10-09 11:47:50 +0800

    fix(cli): 检查cli版本添加超时时间

--- packages/cli/src/doctor/syberhValidator.ts
+++ packages/cli/src/doctor/syberhValidator.ts
@@ -1,7 +1,7 @@
 import * as _ from 'lodash/fp'
 import * as shelljs from 'shelljs';
-import * as  latestVersion from 'latest-version'
-import log from '../util/log';
+import log from '../util/log'
+import latestVersion from '../syberos/latestVersion'
 
 
 // 检查的包的名称
@@ -14,20 +14,29 @@ async function checkSyberhPkg() {
   log.verbose('checkSyberhPkg () start')
   let errorLines: any[] = []
 
+  let remoteVersion
+  try {
+    remoteVersion = await latestVersion(pkgName, 5000)
+    log.verbose('remoteVersion:', remoteVersion)
+  } catch (e) {
+    log.verbose('检查@syberos/cli版本失败', e)
+    return {
+      desc: '检查@syberos/cli版本',
+      lines: [{
+        desc: '连接超时',
+        valid: true,
+        solution: '请检查网络'
+      }]
+    }
+  }
 
-  const remoteVersion = await latestVersion(pkgName);
-  log.verbose('remoteVersion:', remoteVersion)
-  // const pkgs = await npmCheck({
-  //   global: true
-  // }).then(_.invoke('all'))
-  //   .then(_.get('packages'))
   const cmd = 'npm ls @syberos/cli -g';
-  log.verbose('cmd', cmd)
-  // const localVersion = getLocalVersion(pkgs);
+  log.verbose('执行:', cmd)
   shelljs.config.silent = true
   const { stdout } = shelljs.exec(cmd);
   shelljs.config.silent = false
   log.verbose('stdout:', stdout)
+
   let localVersion;
   const arr = stdout.trim().split('@') || [];
   if (arr.length > 1) {
