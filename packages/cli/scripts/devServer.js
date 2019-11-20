b24281604b3c194c5cfb3624289c74d0b8bf0db6
xue99999 <1090374167@qq.com>
2019-11-20 19:08:18 +0800

    fix(cli): 修改热更新参数名错误导致的问题

--- packages/cli/scripts/devServer.js
+++ packages/cli/scripts/devServer.js
@@ -3,7 +3,6 @@ const path = require('path')
 
 const apppath = path.resolve('.')
 // 监测www目录的变动
-const wathcPath = path.join(apppath, 'www')
+const watchPath = path.join(apppath, 'www')
 const port = parseInt(process.argv[2])
-console.log('-------------port---------', port)
-new DevServer({ wathcPath, port })
+new DevServer({ watchPath, port })
