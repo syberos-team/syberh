94e74dbbe28dc959f48dd464e0ea34dcd9ed4b16
xue99999 <1090374167@qq.com>
2019-12-02 18:17:10 +0800

    feat(jsbridge): 修改屏幕旋转API名字

--- packages/jsbridge/src/api/native/app.js
+++ packages/jsbridge/src/api/native/app.js
@@ -1,21 +1,20 @@
-export default function appMixin (hybrid) {
+export default function appMixin(hybrid) {
     const hybridJs = hybrid;
     const innerUtil = hybridJs.innerUtil;
-  
+
     hybridJs.extendModule('app', [
-      {
-        namespace: 'setPageOrientation',
-        os: ['syberos'],
-        runCode (...rest) {
-          // 兼容字符串形式
-          const args = innerUtil.compatibleStringParamsToObject.call(
-            this,
-            rest,
-            'orientation',
-          );
-          hybridJs.callInner.apply(this, args);
+        {
+            namespace: 'setAppOrientation',
+            os: ['syberos'],
+            runCode(...rest) {
+                // 兼容字符串形式
+                const args = innerUtil.compatibleStringParamsToObject.call(
+                    this,
+                    rest,
+                    'orientation',
+                );
+                hybridJs.callInner.apply(this, args);
+            },
         },
-      },
     ]);
-  }
-  
+}
