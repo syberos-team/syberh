54b9733ca6f528d852f066eb706525ebbd642456
Liu Jingpeng <liujingpeng@syberos.com>
2021-11-25 10:33:15 +0800

    fix: 唤起原生应用时不再附带index.html路径

--- packages/jsbridge/src/api/native/packages.js
+++ packages/jsbridge/src/api/native/packages.js
@@ -1,49 +1,11 @@
 import hybridJs from '../../hybrid';
 
 export default function packagesMixin() {
-    const innerUtil = hybridJs.innerUtil;
-
     hybridJs.extendModule('packages', [{
         namespace: 'openUrl',
         os: ['syberos'],
-        defaultParams: {
-            scheme: '',
-            path: 'index.html',
-            params: {},
-        },
-        runCode(...rest) {
-            // 兼容字符串形式
-            const args = innerUtil.compatibleStringParamsToObject(
-                rest,
-                'scheme',
-                'path',
-                'params',
-            );
-
-            hybridJs.callInner.apply(this, args);
-        },
     }, {
         namespace: 'openDocument',
         os: ['syberos'],
-        defaultParams: {
-            sopid: '',
-            uiappid: '',
-            action: 'openPage',
-            path: 'index.html',
-            params: {},
-        },
-        runCode(...rest) {
-            // 兼容字符串形式
-            const args = innerUtil.compatibleStringParamsToObject(
-                rest,
-                'sopid',
-                'uiappid',
-                'action',
-                'path',
-                'params',
-            );
-
-            hybridJs.callInner.apply(this, args);
-        },
     }]);
 }
